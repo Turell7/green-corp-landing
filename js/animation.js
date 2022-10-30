b1974a3efe6c658b34f16a2ef37029afc15f826c
Н С <nikolai3578@gmail.com>
2022-10-30 17:18:47 +0300

    Add form__other-input

--- js/animation.js
+++ js/animation.js
@@ -19,4 +19,24 @@ const element = document.querySelector('.features__clients-count');
 increaseNumerAnimationStep(0, element, 5000);
 }
 initIncreaseNumberAnimation();
-console.log("test");
+
+document.querySelector("#budget").addEventListener('change',
+function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+        const formContainer = document.createElement('div');
+        formContainer.classList.add('form__group');
+        formContainer.classList.add('form__other-input');
+
+        const input = document.createElement('input');
+        input.placeholder = "Введите ваш вариант";
+        input.type = "text";
+
+        formContainer.appendChild(input);
+        document.querySelector(".form form").insertBefore(formContainer, document.querySelector(".form__submit"));
+    }
+    const otherInput = document.querySelector(".form__other-input");
+    if (event.target.value !== 'other' && Boolean(otherInput)) {
+        document.querySelector(".form form").removeChild(otherInput);
+    }
+});
+

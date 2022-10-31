c067cb65cff0523b13ab4917114310f18dfe2e6b
Н С <nikolai3578@gmail.com>
2022-10-31 16:02:38 +0300

    Create smoothScroll for anchors

--- js/animation.js
+++ js/animation.js
@@ -18,7 +18,7 @@ function initIncreaseNumberAnimation() {
 const element = document.querySelector('.features__clients-count');
 increaseNumerAnimationStep(0, element, 5000);
 }
-initIncreaseNumberAnimation();
+// initIncreaseNumberAnimation();
 
 document.querySelector("#budget").addEventListener('change',
 function handleSelectChange(event) {
@@ -32,11 +32,46 @@ function handleSelectChange(event) {
         input.type = "text";
 
         formContainer.appendChild(input);
-        document.querySelector(".form form").insertBefore(formContainer, document.querySelector(".form__submit"));
+        document.querySelector("#form form").insertBefore(formContainer, document.querySelector(".form__submit"));
     }
     const otherInput = document.querySelector(".form__other-input");
     if (event.target.value !== 'other' && Boolean(otherInput)) {
-        document.querySelector(".form form").removeChild(otherInput);
+        document.querySelector("#form form").removeChild(otherInput);
     }
 });
 
+let animationInited = false;
+function updateScroll() {
+    if (window.scrollY > 0) {
+        document.querySelector("header").classList.add("header__scrolled");
+    } else {
+        document.querySelector("header").classList.remove("header__scrolled");
+    }
+
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+
+    if (windowBottomPosition <= countElementPosition && animationInited === false) {
+        animationInited = true;
+        initIncreaseNumberAnimation();
+    }
+}
+
+window.addEventListener("scroll", updateScroll);
+
+function addSmoothScroll(anchor) {
+    anchor.addEventListener("click", onLinkClick);
+};
+
+function onLinkClick(event) {
+    event.preventDefault();
+    document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    addSmoothScroll(anchor);
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
